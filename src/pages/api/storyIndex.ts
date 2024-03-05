1cee0337215291da65199cfac3772953cec295e0
Michael Shilman <shilman@users.noreply.github.com>
2024-03-05 12:52:24 +0800

    Add first draft of story index generation

--- src/pages/api/storyIndex.ts
+++ src/pages/api/storyIndex.ts
@@ -1,6 +1,52 @@
+import path from 'path';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { StoryIndex, storyIndex } from '../../storybook/storyIndex';
+import { readCsf } from '@storybook/csf-tools';
+import type { StoryIndex } from '../../storybook/storyIndex';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<StoryIndex>) {
-  res.status(200).json(storyIndex);
+const relativePattern = /^\.{1,2}([/\\]|$)/;
+/**
+ * Ensures that a path starts with `./` or `../`, or is entirely `.` or `..`
+ */
+export function normalizeStoryPath(filename: string) {
+  if (relativePattern.test(filename)) return filename;
+
+  return `.${path.sep}${filename}`;
+}
+
+const NODE_MODULES_RE = /node_modules/;
+
+/**
+ * Exclude node_modules stories everywhere we call `glob`
+ */
+export const commonGlobOptions = (glob: string) =>
+  NODE_MODULES_RE.test(glob) ? {} : { ignore: ['**/node_modules/**'] };
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<StoryIndex>) {
+  const workingDir = process.cwd();
+  const glob = (await import('globby')).globby;
+  const slash = (await import('slash')).default;
+
+  const specifiers = ['src/storybook/stories/**/*stories.tsx'];
+  const files = await Promise.all(
+    specifiers.map((specifier) => glob(specifier, commonGlobOptions(specifier)))
+  );
+
+  const makeTitle = (userTitle?: string) => userTitle ?? 'FIXME';
+
+  const index = {} as StoryIndex;
+  await Promise.all(
+    files.flat().map(async (file: string) => {
+      const csf = (await readCsf(file, { makeTitle })).parse();
+      Object.entries(csf._stories).forEach(([key, story]) => {
+        index[story.id] = {
+          title: csf.meta.title!,
+          name: story.name!,
+          key: key,
+          importPath: normalizeStoryPath(slash(path.relative(workingDir, file))),
+        };
+      });
+    })
+  );
+
+  res.status(200).json(index);
 }
